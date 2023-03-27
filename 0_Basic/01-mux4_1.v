cf41b4323a3e36ad85246c7c197f39afc8d08786
wanglei <wanglei4work@gmail.com>
2023-03-28 01:24:39 +0800

    init commit

---
+++ 0_Basic/01-mux4_1.v
@@ -0,0 +1,15 @@
+`timescale 1ns/1ns
+
+/* 
+    VL1 四选一多路器
+*/
+
+module mux4_1(
+input [1:0]d1,d2,d3,d0,
+input [1:0]sel,
+output[1:0]mux_out
+);
+//*************code***********//
+
+//*************code***********//
+endmodule
