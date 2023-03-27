03049c21c20e88c26a6afeefcbbbc71dadb9cd7a
wanglei <wanglei4work@gmail.com>
2023-03-28 01:25:52 +0800

    update VL1

--- 0_Basic/01-mux4_1.v
+++ 0_Basic/01-mux4_1.v
@@ -10,6 +10,22 @@ input [1:0]sel,
 output[1:0]mux_out
 );
 //*************code***********//
+wire [1:0] d1, d2, d3, d0;
+wire [1:0] sel;
+wire [1:0] mux_out;
+reg  [1:0] mux_out_reg;
 
+always @(*)
+begin
+    case (sel)
+        2'b00: mux_out_reg = d3;
+        2'b01: mux_out_reg = d2;
+        2'b10: mux_out_reg = d1;
+        2'b11: mux_out_reg = d0;
+        default: mux_out_reg = d3;
+    endcase
+end
+
+assign mux_out = mux_out_reg;
 //*************code***********//
 endmodule
---
+++ 0_Basic/02-Tff_2.v
@@ -0,0 +1,15 @@
+`timescale 1ns/1ns
+
+/* 
+    VL2 异步复位的串联 T 触发器
+*/
+
+module Tff_2 (
+input wire data, clk, rst,
+output reg q  
+);
+//*************code***********//
+
+
+//*************code***********//
+endmodule
