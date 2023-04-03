46595ad5fdb4f6eafededc3c964b9cb8e2eb9cfb
wanglei <wanglei4work@gmail.com>
2023-04-04 00:13:04 +0800

    update VL2

--- 0_Basic/02-Tff_2.v
+++ 0_Basic/02-Tff_2.v
@@ -9,7 +9,31 @@ input wire data, clk, rst,
 output reg q  
 );
 //*************code***********//
+reg q1;
 
+always @(posedge clk or negedge rst) begin
+    if (rst == 1'b0) begin
+        q1 <= 1'b0;
+    end else begin
+        if (data) begin
+            q1 <= ~q1;
+        end else begin
+            q1 <= q1;
+        end
+    end
+end
+
+always @(posedge clk or negedge rst) begin
+    if (rst == 1'b0) begin
+        q <= 1'b0;
+    end else begin
+        if (q1) begin
+            q <= ~q;
+        end else begin
+            q <= q;
+        end
+    end
+end
 
 //*************code***********//
 endmodule
